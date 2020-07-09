66b6e33f0e4abe2bddda00f91e8735f448912d5a
KIIT <asstore3@gmail.com>
2020-07-09 20:29:41 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dicenoleft = Random().nextInt(6), diceright = Random().nextInt(6);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                Randomgenerate();
+              },
+              child: Image.asset('images/dice$diceright.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                Randomgenerate();
+              },
+              child: Image.asset('images/dice$dicenoleft.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  void Randomgenerate(){
+      setState(
+            () {
+          diceright = Random().nextInt(6)+1;
+          dicenoleft = Random().nextInt(6)+1;
+        },
+      );
   }
 }
